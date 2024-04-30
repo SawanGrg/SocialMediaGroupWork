5376c5384b1daeadff5d9965014e5e053e7c4745
Gaurav Gurung <gurungg112@gmail.com>
2024-04-30 09:40:32 +0545

    Merge with rishikesh 1

--- obj/Debug/net8.0/GroupCoursework.AssemblyInfo.cs
+++ obj/Debug/net8.0/GroupCoursework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroupCoursework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0982190877e3c75130a5d479c4507d4d8a8c55aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a475aac0fd5e3fc282e1c380f64c895bd961bf09")]
 [assembly: System.Reflection.AssemblyProductAttribute("GroupCoursework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GroupCoursework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
