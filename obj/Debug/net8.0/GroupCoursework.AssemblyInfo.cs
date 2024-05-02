7473b93db8d1ef8fd1590997a0c083d46376b7cd
SawanGurung <you@example.com>
2024-05-02 17:02:55 +0545

    obj file commit

--- obj/Debug/net8.0/GroupCoursework.AssemblyInfo.cs
+++ obj/Debug/net8.0/GroupCoursework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroupCoursework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f6cf949700f88018302744ebf5cf4ec6bdfac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0260b77e611f15da2172262d4d4cec5f9f75de")]
 [assembly: System.Reflection.AssemblyProductAttribute("GroupCoursework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GroupCoursework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
